5377b133b9652a64c5906a7f05c4969ac8c4ccba
Kyle Zheng <kylezheng73@gmail.com>
2024-06-23 07:20:36 -0400

    store in cookies

--- app/auth.py
+++ app/auth.py
@@ -55,7 +55,13 @@ async def auth_google(code: str):
           session.commit()
     
     # should redirect immediately, but client side i guess
-    return {"access_token": access_token, "id_token": id_token}
+# {"access_token": access_token, "id_token": id_token}
+    resp = RedirectResponse("/")
+    resp.set_cookie(key="access_token", value=access_token)
+    resp.set_cookie(key="id", value=id_token)
+# headers={"Set-Cookie": f"access_token={access_token}; HttpOnly; Secure",
+#                                           "Set-Cookie": f"id_token={id_token}; HttpOnly; Secure" }
+    return resp
 
 async def get_current_user(id_token: Annotated[str, Cookie()]):
     payload = base64.b64decode(id_token.split(".")[1] + "==")
